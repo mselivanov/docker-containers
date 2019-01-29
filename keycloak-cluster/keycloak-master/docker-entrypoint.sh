735ab915fa6a9cfa67081bd63589f398bdd4ebf1
Maksim Selivanau <maksim.selivanau@cantire.com>
2019-01-29 11:22:10 +0300

    Add keycloak cluster example

---
+++ keycloak-cluster/keycloak-master/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##################
+# Add admin user #
+##################
+
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    keycloak/bin/add-user-keycloak.sh --sc /opt/jboss/keycloak/domain/servers/server-one/configuration --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+fi
+
+keycloak/bin/add-user-keycloak.sh --sc /opt/jboss/keycloak/domain/servers/server-one/configuration --user --password 
+
+##################
+# Add admin management user #
+##################
+if [ $KEYCLOAK_MGMT_USER ] && [ $KEYCLOAK_MGMT_PASSWORD ]; then
+    keycloak/bin/add-user.sh --user $KEYCLOAK_MGMT_USER --password $KEYCLOAK_MGMT_PASSWORD --enable
+fi
+
+##################
+# Start Keycloak #
+##################
+
+exec /opt/jboss/keycloak/bin/domain.sh $@
+exit $?
